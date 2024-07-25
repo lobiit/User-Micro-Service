f19534e35a7cb60b72b6c12c3a969f54bc5c857a
John Lobi Tumbo <john.lobi.tumbo@gmail.com>
2024-07-25 11:52:48 +0300

    feat: user creation and listing

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from django.utils.decorators import classonlymethod
+from asgiref.sync import sync_to_async
+from .models import UserProfile
+from .serializer import UserProfileSerializer
 
-# Create your views here.
+
+class UserProfileListCreateView(View):
+    @classonlymethod
+    async def get(cls, request):
+        user_profiles = await sync_to_async(UserProfile.objects.all)()
+        serialized_data = UserProfileSerializer().serialize(user_profiles)
+        return JsonResponse(serialized_data, safe=False)
+
+    @classonlymethod
+    async def post(cls, request):
+        data = request.POST
+        user_profile = await sync_to_async(UserProfile.objects.create)(
+            username=data['username'],
+            email=data['email'],
+            bio=data['bio']
+        )
+        serialized_data = UserProfileSerializer().serialize([user_profile])
+        await send_user_profile.delay(serialized_data)
+        return JsonResponse(serialized_data, safe=False)
